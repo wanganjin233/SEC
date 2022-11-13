7041d9cd043a941a45f493ea1671df0f7f710257
wanganjin <1046477311@qq.com>
2022-11-13 11:03:25 +0800

    长度类型

---
+++ SEC.Drivers/SEC.Driver/Enum/LengthTypeEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace SEC.Driver
+{
+    public enum LengthTypeEnum
+    {
+        Byte,
+        UShort,
+        ReUShort,
+        Uint,
+        HUint,
+        ReUint,
+        ReHUint
+    }
+}
